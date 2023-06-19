37f2e4507fabcc9abbf42308d56039889817ebb9
lingyundai <lingyundaiseven@gmail.com>
2023-06-19 18:59:42 -0400

    finished container design,font imports,added form labels

--- src/containers/CalculatorContainer.js
+++ src/containers/CalculatorContainer.js
@@ -2,7 +2,11 @@ import Calculator from "../components/Calculator";
 
 function CalculatorContainer() {
     return (
-        <Calculator></Calculator>
+        <div class="flex justify-center items-center h-screen w-screen bg-stone-100">
+            <div class="h-1/2 w-9/12 max-w-3xl max-h-128 bg-white rounded-br-large rounded-t-default rounded-bl-default">
+                <Calculator />
+            </div>
+        </div>
     )
 }
 
