3f74d9642a629681596a159272db990fb8b14c7e
lingyundai <lingyundaiseven@gmail.com>
2023-06-26 13:56:11 -0400

    input text styling

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,15 +4,21 @@ function Calculator() {
             <div class="flex pt-5 space-x-4 justify-center">
                 <div class="w-1/4">
                     <label>DAY</label>
-                    <input name="day" class="border rounded-md w-full py-3.5 focus:outline-none focus:border-violet-500" />
+                    <input name="day" class="border rounded-md w-full
+                         text-lg py-2 text-black tracking-wider font-poppins indent-3
+                         focus:outline-none focus:border-violet-500" />
                 </div>
                 <div class="w-1/4">
                     <label>MONTH</label>
-                    <input name="month" class="border rounded-md w-full py-3.5 focus:outline-none focus:border-violet-500" />
+                    <input name="month" class="border rounded-md w-full 
+                        text-lg py-2 text-black tracking-wider font-poppins indent-3
+                        focus:outline-none focus:border-violet-500" />
                 </div>
                 <div class="w-1/4">
                     <label>YEAR</label>
-                    <input name="year" class="border rounded-md w-full py-3.5 focus:outline-none focus:border-violet-500" />
+                    <input name="year" class="border rounded-md w-full 
+                        text-lg py-2 text-black tracking-wider font-poppins indent-3
+                        focus:outline-none focus:border-violet-500" />
                 </div>
             </div>
             <div class="pt-16 px-7">
