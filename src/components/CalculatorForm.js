93aeaf011564b9b0862166a59a3f5e3ae516c2bf
lingyundai <lingyundaiseven@gmail.com>
2023-07-03 17:28:20 -0400

    set states for values

--- src/components/CalculatorForm.js
+++ src/components/CalculatorForm.js
@@ -1,44 +1,52 @@
 import { useState } from "react";
-import { add } from 'date-fns';
+import DateUtils from "../utils/DateUtils";
 
 function CalculatorForm() {
-    const [day, setDay] = useState(null);
-    const [month, setMonth] = useState(null);
-    const [year, setYear] = useState(null);
+    const defaultValues = {
+        year: '',
+        month: '',
+        day: '',
+    };
 
-    function statesBundle(day, month, year) {
-        setDay(day);
-        setMonth(month);
-        setYear(year);
-    }
+    const [inputFields, setInputFields] = useState(defaultValues);
+
+    const { year, month, day } = inputFields;
+
+    const handleChange = (e) => {
+        setInputFields({
+            ...inputFields, 
+            [e.target.name]: e.target.value,
+        });
+      };
+
+    console.log("fields: ", inputFields);
 
     return (
         <form class="max-w-fit pt-6 font-poppins font-semibold text-[11px] tracking-[0.2em]">
             <div class="flex pt-5 space-x-4 justify-center text-stone-500">
                 <div class="w-1/4">
                     <label>DAY</label>
-                    <input value={day}
-                        name="day" 
-                        class="border rounded-md w-full
+                    <input required value={day} type="text" onChange={handleChange}
+                        name="day" class="border rounded-md w-full
                         text-lg py-2 text-black tracking-wider indent-3
                         focus:outline-none focus:border-violet-500" />
                 </div>
                 <div class="w-1/4">
                     <label>MONTH</label>
-                    <input value={month}
+                    <input required value={month} type="text" onChange={handleChange}
                         name="month" class="border rounded-md w-full 
                         text-lg py-2 text-black tracking-wider indent-3
                         focus:outline-none focus:border-violet-500" />
                 </div>
                 <div class="w-1/4">
                     <label>YEAR</label>
-                    <input value={year}
+                    <input required value={year} type="text" onChange={handleChange}
                         name="year" class="border rounded-md w-full 
                         text-lg py-2 text-black tracking-wider indent-3
                         focus:outline-none focus:border-violet-500" />
                 </div>
             </div>
-            <div class="pt-16 px-7 inline-flex items-center justify-center w-full" onClick={() => statesBundle(day, month, year)}>
+            <div class="pt-16 px-7 inline-flex items-center justify-center w-full">
                 <hr class="w-full"/>
                 <svg class="w-14 h-14 bg-violet-500 rounded-full absolute" viewBox="-32 15 110 15" xmlns="http://www.w3.org/2000/svg">
                     <path fill="none" stroke="#FFF" stroke-width="3"
---
+++ src/utils/DateUtils.js
@@ -0,0 +1,9 @@
+// import { format } from 'date-fns';
+
+// function DateUtils(year, month, day) {
+//     const result = format(new Date(year, month, day), 'MM/dd/yyyy');
+//     console.log(result);
+//     return result;
+// }
+
+// export default DateUtils;
