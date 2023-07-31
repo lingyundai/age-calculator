94cf1a2dcfc5d35a09c4a536255a316671e4b067
lingyundai <lingyundaiseven@gmail.com>
2023-07-31 17:55:46 -0400

    added somelogic for age calculation

--- src/utils/DateUtils.js
+++ src/utils/DateUtils.js
@@ -1,15 +1,51 @@
-import { format, intervalToDuration, parse } from 'date-fns';
+import moment from "moment";
 
-function DateUtils() {
-    // const birthDate = `${month}-${day}-${year}`;
-    // console.log("here: ", birthDate);
+export function DateUtils(day, month, year) {
 
-    // const today = new Date();
+    let diffMonth;
+    let diffYear;
+    let diffDay;
+    let dayByMonth;
+    
+    const inputString = `${year}/${month}/${day}`;
+    console.log("birth: ", inputString);
 
-    // console.log(intervalToDuration({ start: new Date(birthDate), 
-    //     end: today }));
+    const today = moment(new Date()).format("YYYY/MM/DD");
+    console.log("today ", today);
 
-    // // return { years, months, days };
+    diffYear =  Math.floor(moment.duration(moment(today, 'YYYY/MM/DD')
+        .diff(moment(inputString, 'YYYY/MM/DD'))).asYears());
+    console.log("diffyear ", diffYear);
+
+    diffDay = Math.floor(moment.duration(moment(today, 'YYYY/MM/DD')
+        .diff(moment(inputString, 'YYYY/MM/DD'))).asDays());
+    console.log("diffday: ", diffDay);
+
+    const daysPerMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    if (moment([year]).isLeapYear()) {
+        console.log("1111111");
+    }
+
+    // diff year < 1
+    // diff year > 1
+      // diff month > 1
+      // diff month < 1
+
+    if (diffYear < 1) {
+        diffYear = 0;
+        diffMonth = moment().diff(inputString, 'months');
+        console.log("diffmonth ", diffMonth);
+        console.log("diffyear1 ", diffYear);
+        // diff month will only be less than 12 because if it is over 12 it will be considered a year
+        for (var i=0; i<diffMonth; i++) {
+            dayByMonth += daysPerMonth[i]
+            console.log("here:", dayByMonth);
+          }
+    } else {
+        const diffYearInMonth = diffYear * 12
+        diffMonth = (moment().diff(inputString, 'months')) - diffYearInMonth;
+        console.log("diffmonth ", diffMonth);
+    } 
+
 }
-
-export default DateUtils;
